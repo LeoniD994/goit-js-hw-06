b4d98bce11dc862d8d743dc4df0600edb5dbb97b
Leonid Zelov <112327440+LeonidZelov@users.noreply.github.com>
2023-02-08 14:49:16 +0200

    fix now

--- js/task-05.js
+++ js/task-05.js
@@ -1,12 +1,12 @@
 const textInput = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
 
-textInput.addEventListener('input', changeName);
-function changeName(event) {
+const currentInput = (event) => {
     output.textContent = event.currentTarget.value;
     
-    
-    if (!event.currentTarget.value){
+    if (event.currentTarget.value === '') {
         output.textContent = "Anonymous";
     }
 };
+textInput.addEventListener('input', currentInput);
+
--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,6 @@
-const inputSize = document.querySelector('input#font-size-control');
-const inputText = document.querySelector('span#text');
+const inputSize = document.querySelector('#font-size-control');
+const inputText = document.querySelector('#text');
 
-inputSize.addEventListener('input', event => {
+inputText.style.fontSize = inputSize.value + `px`;
 
-    inputText.style.fontSize = `${event.target.value}px`;
-}
-)
+inputSize.addEventListener("input", event => inputText.style.fontSize = event.currentTarget.value + `px`);
